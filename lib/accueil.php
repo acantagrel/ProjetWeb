24c4a15ce5a6b7499a110cecf87419f049394ee3
Unknown <acantagrel@ensc.fr>
2018-02-20 21:37:21 +0100

    modif accueil

--- lib/accueil.php
+++ lib/accueil.php
@@ -17,23 +17,36 @@
             include "../includes/navBar.php";
         ?>
 
-        <div>
-            <img src="../images/logoIRAEER.png" class="jumbotron"/>
-            <br/> Bienvenue sur IRAEER, le site qui vous permet d'évaluer la qualité de vos sites web !
-            <br/> Pour cela, rien de plus simple, créer un compte (c'est gratuit !) ou connectez-vous et vous pourrez alors réaliser 
-            des campagnes de tests pour évaluer le contenu, l'attrait et l'utilisabilité de votre site web.
-            En créant un compte, vous aurez accès à :
-            <ul>
-                <li></li>
-                <li></li>
-            </ul>
+        <div class="jumbotron">
+            <div class="row">
+                <div class="col-lg-offset-1 col-lg-2">
+                    <img src="../images/logoIRAEER.png" class="img-responsive"/>
+                </div>
+                <div class="col-lg-6">
+                    <p class="alinea"> 
+                        <br/> Bienvenue sur IRAEER, le site qui vous permet d'évaluer la qualité de vos sites web !
+                        <br/> Pour cela, rien de plus simple, créer un compte (c'est gratuit !) ou connectez-vous et vous pourrez alors réaliser 
+                        des campagnes de tests pour évaluer le contenu, l'attrait et l'utilisabilité de votre site web.
+                        <br/>En créant un compte, vous aurez accès à :
+                        <ul class="accueil">
+                            <li> la création de campagnes de tests,</li> 
+                            <li> aux réponses à vos campagnes et leurs analyses, </li>
+                            <li> à la liste des campagnes ouvertes à tous et vous aurez la possibilité d'y répondre. </li>
+                        </ul>
+                    </p>
+                </div>
+                <div class="col-lg-2 col-lg-offset-1">
+                    <button>1</button>
+                    <button>2</button>
+                </div>
+            </div>
         </div>
         
         
         <!-- plugins bootstrap -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script src="lib/bootstrap/js/bootstrap.min.js"></script>
-        <script src="lib/jquery/jquery.min.js"></script>
+        <script src="../bootstrap/js/bootstrap.min.js"></script>
+        <script src="../jquery/jquery.min.js"></script>
 
         <?php
             include "../includes/footer.php";
