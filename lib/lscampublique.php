0535aaade937013f462c44b6369f7769fef2b534
Alice <acantagrel@ensc.fr>
2018-03-05 11:06:04 +0100

    ajout icone logo + creation
    page mesrep et liste des campagnes

--- lib/lscampublique.php
+++ lib/lscampublique.php
@@ -5,13 +5,12 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link rel="icon" href="../images/logoIRAEER.png"  type="image/png">
-        
+        <link rel="icon" href="../images/logoIRAEER.png"  type="image/png">
+		
 		<!-- Attention au chemin ! -->
         <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
 		<link href="../css/iraeer.css" rel="stylesheet">
         <title>IRAEER</title>
-
     </head>
     <body>
         <?php 
@@ -19,15 +18,12 @@
             include "../includes/connect.php";
         ?>
 
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-offset-2 col-lg-8 col-lg-offset-2">
-                    <a href="https://uxmind.eu/2014/06/23/attrakdiff-evaluation-quantitative-ux/">Pour en savoir plus</a>
-                </div>
-            </div>
-        </div>
 
 
+
+
+
+        
         <!-- plugins bootstrap -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="../bootstrap/js/bootstrap.min.js"></script>
