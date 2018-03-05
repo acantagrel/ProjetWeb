1a98b3e31d64bad63503597a9603fca339b0eddb
Alice <acantagrel@ensc.fr>
2018-03-06 00:40:26 +0100

    [lscampublique] ajout requete BDD
    lis le titre la description et les auteurs de la campagne

--- lib/lscampublique.php
+++ lib/lscampublique.php
@@ -21,38 +21,79 @@
         <div class="row">
             <div class="col-lg-offset-1 col-lg-8">
                 <h3 style="text-align:center;">Campagnes publiques</h3>
+                <br/><br/>
                 <div>
                     <?php
                         //BDD
-
-                        $req ="SELECT profil.nom, profil.prenom FROM campagne,profil WHERE campagne.id_exp1=profil.id or campagne.id_exp2=profil.id or campagne.id_3=profil.id";
-                        $res = $BDD->query($req);
-                        while($campagne=$res->fetch())
+                        
+                        //requete titre 
+                        $numeroCampagne = 0;
+                        function presCamp ($numCampagne) 
                         {
-                            //$titre=$campagne['titre'];
-                            //$description = $campagne['description'];
-                            $auteur1= $campagne['nom']+$campagne['prenom'];
-                         //   $auteur2=$campagne['id_exp2'];
-                          //  $auteur3=$campagne['id_exp3'];
-                           // $auteur4=$campagne['id_exp4'];
-                            //$auteur5=$campagne['id_exp5'];
-                            ?> 
-                        <div>
-                            <?= "Auteurs = $auteur1, $auteur2, $auteur3"; ?>
-                        </div>
-
-                        <?php 
+                            $BDD = new PDO("mysql:host=localhost;dbname=iraeer;charset=utf8","BouCan","surprise",array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+                            $req3 = "SELECT * FROM campagne WHERE id=$numCampagne";
+                            $res3 = $BDD->query($req3);
+                            while($campagne=$res3->fetch())
+                            {
+                                $titre = $campagne['titre'];
+                                $desc = $campagne['description'];
+                                ?> 
+                                <h4><?=$titre?></h4>
+                                <div style="text-indent: 15px;"><?=$desc?></div>
+                                <?php
+                            }
                         }
                         
-                        if (array_key_exists('login',$_SESSION) and array_key_exists('mdp',$_SESSION))
+                             
+
+
+                        function requeteAuteur($id_exp,$numeroCampagne)
                         {
-                            ?>
-                            <form action="#">
-                                <input type="submit" value="Participer" />
-                            </form>
-                        <?php
+                            //retourne le nom du profil associé à l'id_expérimentateur de la table campagne
+                            $BDD = new PDO("mysql:host=localhost;dbname=iraeer;charset=utf8","BouCan","surprise",array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+                            $req ="SELECT nom, prenom FROM profil, campagne WHERE profil.id = $id_exp AND campagne.id=$numeroCampagne"; 
+                            $res = $BDD->query($req);
+                            while($camp=$res->fetch())
+                            {
+                                $auteurP= $camp['prenom'];
+                                $auteurN= $camp['nom'];
+                                $auteur = "$auteurP $auteurN";
+                            }  
+                            return $auteur;
+                        }
+
+                        //requete qui cherche les colonnes 'id_experimentateur' qui possèdent une valeur et affiche le nom correspondant à l'id_exp
+                        for ($numeroCampagne=0; $numeroCampagne<2;$numeroCampagne++)
+                        //ATTENTION !!!!!!!!!!!!!!!!!!!!!!! Faut trouver comment limiter le numero de campagnes !
+                        {
+                            presCamp($numeroCampagne);
+                            print("Auteur(s) : ");
+                            for ($i=1; $i<=5; $i++)
+                            {
+                                $req2 ="SELECT * FROM campagne WHERE id_exp$i IS NOT NULL GROUP BY id HAVING id=$numeroCampagne"; 
+                                $res2 = $BDD->query($req2);
+                                while($campagne=$res2->fetch())
+                                {
+                                    print(requeteAuteur("campagne.id_exp$i",$numeroCampagne)." ; ");
+                                }
+                            }
+                            if (array_key_exists('login',$_SESSION) and array_key_exists('mdp',$_SESSION))
+                            {
+                                ?>
+                                <br/><br/>
+                                <form action="#">
+                                    <input type="submit" value="Participer" />
+                                </form>
+                            <?php
+                            }
+                            ?> 
+                            <br/><br/>
+                            <?php
                         }
                         ?>
+                        
+                    
+
                 </div>
             </div>
             <div class="col-lg-3">
